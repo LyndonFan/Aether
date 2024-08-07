c06795ae5e8152ae06600a528e1dcbd73f844789
Lyndon Fan <lyndon0808@gmail.com>
2024-08-07 20:48:44 +0100

    feat: allow create >1 note

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,22 +42,29 @@ export default function App(): React.JSX.Element {
 		}
 	}, []);
 
-	const createNewNote = (
-		initialNoteContent: string | null
+	const createNewNotes = (
+		initialNoteContents: (string | null)[]
 	) => {
-		const newNote: Note = {
-			id: uuid(),
-			title: "",
-			content:
-				initialNoteContent || "# New Note\n\nHello world!",
-		};
-		newNote.title = newNote.content.split("\n", 1)[0];
-		setNotes(new Map([...notes, [newNote.id, newNote]]));
-		localStorage.setItem(
-			NOTE_FIELD_PREFIX + newNote.id,
-			JSON.stringify(newNote)
+		const newNotes: Map<string, Note> = new Map();
+		for (const content of initialNoteContents.values()) {
+			const newNote: Note = {
+				id: uuid(),
+				title: "",
+				content: content || "# New Note\n\nHello world!",
+			};
+			newNote.title = newNote.content.split("\n", 1)[0];
+			localStorage.setItem(
+				NOTE_FIELD_PREFIX + newNote.id,
+				JSON.stringify(newNote)
+			);
+			newNotes.set(newNote.id, newNote);
+		}
+		setNotes(
+			new Map([...notes.entries(), ...newNotes.entries()])
 		);
-		setCurrentNoteId(newNote.id);
+		if (newNotes.size === 1) {
+			setCurrentNoteId(newNotes.keys().next().value);
+		}
 	};
 
 	const selectNote = (id: string) => {
@@ -115,7 +122,7 @@ export default function App(): React.JSX.Element {
 
 	return (
 		<div className="flex">
-			<Sidebar createNewNote={createNewNote} />
+			<Sidebar createNewNotes={createNewNotes} />
 			<NoteList notes={noteList} selectNote={selectNote} />
 			{currentNote && (
 				<NotePage
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 interface SidebarProps {
-	createNewNote: (
-		initialNoteContent: string | null
+	createNewNotes: (
+		initialNoteContents: (string | null)[]
 	) => void;
 }
 
 export default function Sidebar({
-	createNewNote,
+	createNewNotes,
 }: SidebarProps): React.JSX.Element {
 	const BUTTON_STYLING_CLASS_NAME =
 		"bg-blue-500 text-white w-full py-2 rounded";
@@ -15,7 +15,7 @@ export default function Sidebar({
 	const newNoteFromClipboard = () => {
 		navigator.clipboard
 			.readText()
-			.then((content) => createNewNote(content));
+			.then((content) => createNewNotes([content]));
 	};
 
 	const onReceiveFileInput = (event: EventTarget) => {
@@ -49,7 +49,7 @@ export default function Sidebar({
 	return (
 		<div className="max-w-1/5 h-screen bg-gray-200 p-4 space-y-4">
 			<button
-				onClick={() => createNewNote(null)}
+				onClick={() => createNewNotes([null])}
 				className={BUTTON_STYLING_CLASS_NAME}
 			>
 				New Blank Note
